ec65d1ab172ab0cd35e2e5fbc7684a17bb287bc6
Dirk-Bender <dirk.bender@student.uni-siegen.de>
2026-06-11 21:34:13 +0200

    new datatype "Shift" defined some additional input files (parameters) -> prototypes/templates only

---
+++ coding/src/Shift.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field   # Import the dataclass decorator and field helper to define lightweight immutable or mutable data containers and to provide default factory for mutable defaults
+import pandas as pd                       # Import pandas under the conventional alias pd to read the CSV into a DataFrame and to work with tabular data
+import datetime as dt
+
+
+# Dataclass definition -------------------------------------------------------
+
+@dataclass
+class Shift:
+    # data type class for shifts
+    shift_id: str                           # unique shift ID
+    description: str                            # clear-text description of the shif
+    weekdays: list[str]                     # weekdays for which the shift is relevant
+    start: dt.time                   # start time of the shift (hh:mm)
+    end: dt.time                     # end time of the shift (hh:mm)
+    required_staff: int           # number of worker required for the shift
+    shift_class: int              # shift class relecting attractiveness of the shift (values 1-10)
+    shift_work_time_assignment: str  # 
+    is_work_shift: bool           # boolean to reflect if a shift is treated as work-shift or not (eg "free_day" is False)
+    required_qualification: str  # qualification needs for the shift (relevant for mapping suitable staff members)
+    
+
