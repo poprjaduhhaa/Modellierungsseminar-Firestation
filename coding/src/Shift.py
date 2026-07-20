582b2af6d41f67e4582d2eb8003c72fdb35c034b
Dirk-Bender <dirk.bender@student.uni-siegen.de>
2026-07-20 20:36:50 +0200

    comments in Shift.py only

--- coding/src/Shift.py
+++ coding/src/Shift.py
@@ -8,16 +8,16 @@ import datetime as dt
 @dataclass
 class Shift:
     # data type class for shifts
-    shift_id: str                           # unique shift ID
-    description: str                            # clear-text description of the shif
-    weekdays: list[str]                     # weekdays for which the shift is relevant
-    start: dt.time                   # start time of the shift (hh:mm)
-    end: dt.time                     # end time of the shift (hh:mm)
-    required_staff: int           # number of worker required for the shift
-    shift_class: int              # shift class relecting attractiveness of the shift (values 1-10)
-    shift_work_time_assignment: int  # 
-    is_work_shift: bool           # boolean to reflect if a shift is treated as work-shift or not (eg "free_day" is False)
-    required_qualification: str  # qualification needs for the shift (relevant for mapping suitable staff members)
+    shift_id: str           # unique shift ID
+    description: str        # clear-text description of the shif
+    weekdays: list[str]     # weekdays for which the shift is relevant
+    start: dt.time          # start time of the shift (hh:mm)
+    end: dt.time            # end time of the shift (hh:mm)
+    required_staff: int     # number of worker required for the shift
+    shift_class: int        # shift class relecting attractiveness of the shift (values 1-10)
+    shift_work_time_assignment: int     # working time included in the shift
+    is_work_shift: bool                 # boolean to reflect if a shift is treated as work-shift or not (eg "free_day" is False)
+    required_qualification: str         # qualification needs for the shift (relevant for mapping suitable staff members)
 
     # Anwesenheitszeit in Stunden (für die Berechnung der Soll-Arbeitszeit)
 def shift_duration_hours(shift) -> float:
